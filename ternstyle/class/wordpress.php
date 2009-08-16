be1efd9bd734f4729939f0c1314f6943f5f47fb3
mpraetzel <mpraetzel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-16 19:29:54 +0000

    tagging version 2.3
    git-svn-id: https://plugins.svn.wordpress.org/members-list/tags/2.3@146422 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ternstyle/class/wordpress.php
+++ ternstyle/class/wordpress.php
@@ -6,6 +6,8 @@
 ////		1) ternstyle's wordpress functions
 ////	Account:
 ////		Added on April 21st 2009
+////	Version:
+////		0.5
 ////
 ////	Written by Matthew Praetzel. Copyright (c) 2009 Matthew Praetzel.
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -39,7 +41,30 @@ class ternWP {
 		elseif(isset($o) and (empty($o) or $v) and !empty($d)) {
 			update_option($n,$d);
 		}
+		elseif(isset($o) and !empty($d)) {
+			foreach($d as $k => $v) {
+				if(!isset($o[$k])) {
+					$o[$k] = $v;
+				}
+			}
+			update_option($n,$o);
+		}
 		return get_option($n);
+	}
+	function updateOption($n,$d,$w) {
+		$o = $this->getOption($n,$d);
+		if(wp_verify_nonce($_REQUEST['_wpnonce'],$w) and $_REQUEST['action'] == 'update') {
+			$f = new parseForm('post','_wp_http_referer,_wpnonce,action,submit');
+			foreach($o as $k => $v) {
+				if(empty($f->a[$k])) {
+					$f->a[$k] = $v;
+				}
+			}
+			return $this->getOption($n,$f->a,true);
+		}
+		else {
+			return $this->getOption($n,$d);
+		}
 	}
 
 }
